1b73b9e59f7e95eca0fc0be84a6430c6707df30b
Spycrab-db <gavinyan2004@gmail.com>
2023-06-01 21:22:26 -0400

    Implemented sort, rowSwap and printMatrix

---
+++ matrix.js
@@ -0,0 +1,44 @@
+const { Fraction } = require('mathjs');
+
+Fraction.prototype.toString = function () {
+    if (this.d === 1){
+        return `${this.n * this.s}`;
+    }
+    return `${this.n * this.s}/${this.d}`;
+};
+
+let matrix = [
+    [Fraction('0'), Fraction('2'), Fraction('2'), Fraction('5')],
+    [Fraction('2'), Fraction('1'), Fraction('3'), Fraction('2')],
+    [Fraction('0'), Fraction('0'), Fraction('1'), Fraction('4')]
+]
+
+function sort(matrix) {
+    for (let i = 0; i < 3; i++) {
+        if (!matrix[i][0].equals(0)) {
+            rowSwap(matrix, 0, i);
+            break;
+        }
+    }
+    for (let i = 1; i < 3; i++) {
+        if (!matrix[i][1].equals(0)) {
+            rowSwap(matrix, 1, i);
+            break;
+        }
+    }
+}
+
+function rowSwap(matrix, row1Index, row2Index){
+    let row = matrix[row1Index];
+    matrix[row1Index] = matrix[row2Index];
+    matrix[row2Index] = row;
+}
+
+function printMatrix(matrix){
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 4; j++) {
+            process.stdout.write(matrix[i][j].toString());
+        }
+        console.log();
+    }
+}
