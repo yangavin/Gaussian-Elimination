2c9f99aee197501a0d870fe83e0571a7c6287129
Spycrab-db <gavinyan2004@gmail.com>
2023-06-02 21:16:17 -0400

    Implemented script for updating row echelon in real-time

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
+//Get all necessary elements
+const rowEchelonValues = Array.from(document.querySelectorAll('.row-echelon-value'));
+
 const inputMatrix = [];
-for (let i = 0; i < inputs.length; i += 4){
-    inputMatrix.push(inputs.slice(i, i+4));
+for (let i = 0; i < inputs.length; i += 4) {
+    inputMatrix.push(inputs.slice(i, i + 4));
+}
+
+const outputMatrix = [];
+for (let i = 0; i < rowEchelonValues.length; i += 4) {
+    outputMatrix.push(rowEchelonValues.slice(i, i + 4));
 }
 
 const matrix = [
@@ -10,23 +18,36 @@ const matrix = [
 ];
 
 let rowEchelon;
+let isSolved = false;
 
-for (let i = 0; i < 3; i++){
-    for (let j = 0; j < 4; j++){
+//Loop through all inputs and add corresponding event listeners
+for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 4; j++) {
         let currentInput = inputMatrix[i][j];
-        currentInput.addEventListener('input', ()=>{
+        currentInput.addEventListener('input', () => {
+            //Delete all invalid characters
             currentInput.value = currentInput.value.replace(/[^0-9/.-]/g, '');
+            //Default value of 0
             let value = Fraction(0);
-            if (currentInput.value !== ''){
-                try{
+            if (currentInput.value !== '') {
+                try {
                     value = Fraction(currentInput.value);
-                }catch{}
+                } catch { }
             }
             matrix[i][j] = value;
             rowEchelon = toRowEchelon(matrix);
+            updateRowEchelon();
+            //Debugging
             printMatrix(matrix);
             console.log(' ');
         })
     }
 }
 
+function updateRowEchelon() {
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 4; j++) {
+            outputMatrix[i][j].textContent = rowEchelon[i][j].toString();
+        }
+    }
+}
